85259536999b91a52ad34a1681ff3705760a0e35
Jedemajed4617 <90614493+Jedemajed4617@users.noreply.github.com>
2024-09-24 00:23:16 +0200

    Update #1.4 - Added popup
    - Made a responsive popup for project cards to hold more information

--- src/school.js
+++ src/school.js
@@ -1,69 +1,116 @@
- import React from 'react';
-import './school.css'; // Make sure to import your CSS file
+import React, { useState } from 'react';
+import './school.css'; 
 
 function School() {
+    const [popupVisible, setPopupVisible] = useState(false);
+    const [selectedProject, setSelectedProject] = useState(null);
+
     const projects = [
         {
-            title: 'Spatify',
-            description: 'Spotify website clone, php/css/js/sql',
+            title: 'Spatify (spotify clone)',
             link: 'https://tgsoftware.services/spatify',
             image: 'https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs=w240-h480-rw',
+            description: "This is a website a made for a school assignment. This website has it's own small library of songs that you can acces. This was to train my php skills in sorting, searching and adding songs with a small custom csm system.",
+            tags: "PHP, HTML, CSS, JS",
         },
         {
             title: 'Daily Paper Clone',
-            description: 'Daily paper website clone, php/css/js/sql',
             link: 'https://tgsoftware.services/dailypaper',
             image: 'https://www.asphaltgold.com/cdn/shop/files/ca802008830dc007677dcccde8cf41e179b52d66_2322035_Daily_Paper_Ezar_Zip_Hoodie_Black_os_3_768x768.jpg?v=1700042914',
+            description: "This was an assignment for school I made with php, This is a full scale daily paper clone with working cms, login, dipuste system, review system and much more. Had so much fun creating this prject because it enhanced my capability to write php.",
+            tags: "PHP, HTML, CSS, JS",
         },
         {
             title: 'Tulpreizen',
-            description: 'Tulip Travel website, html/css',
             link: 'https://tgsoftware.services/tulpreizen',
             image: 'https://www.whiteflowerfarm.com/mas_assets/cache/image/9/4/e/b/38123.Jpg',
+            description: "This was a small website for school that shows of a bit of scss skills i used for it. Nothing much to it other then nice!",
+            tags: "HTML, SCSS, CSS",
         },
         {
             title: 'Font Showcase',
-            description: 'Font showcase website, html/css/js',
             link: 'https://tgsoftware.services/font-showcase',
             image: 'https://img.freepik.com/free-vector/creative-halloween-alphabet-design_23-2147932875.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1696809600&semt=ais',
+            description: "This was a website created for previewing differnt font styles with js. Anything you type into the bar will show it in the font listed.",
+            tags: "HTML, CSS, JS",
         },
         {
-            title: 'NVVN Website assignment',
-            description: 'Website for a client with php, php/css/js/sql',
+            title: 'NVVN Website',
             link: 'https://tgsoftware.services/nvvn',
             image: 'https://nvvn.nl/wp-content/uploads/2021/06/NVVN-favicon.png',
+            description: "This is a website made for the official NVVN. We needed to show what we had learned the previous years and just make a website for them.",
+            tags: "PHP, HTML, CSS, JS",
         },
         {
             title: 'Qr Code',
-            description: 'Website for an assignment for school. simple styling, advanced backend, php/css/js/sql',
             link: 'https://tgsoftware.services/qrcode',
             image: 'https://media.istockphoto.com/id/1347277567/vector/qr-code-sample-for-smartphone-scanning-on-white-background.jpg?s=612x612&w=0&k=20&c=PYhWHZ7bMECGZ1fZzi_-is0rp4ZQ7abxbdH_fm8SP7Q=',
+            description: "This was a really fun project. This website i made coukld generate qr codes after you had taken a picture with any device on the website and the if you had scan the QR-code you were redirected to a different page to collect your photo.",
+            tags: "PHP, HMTL, CSS, JS",
         },
         {
-            title: 'Rabbit (infinite scroller)',
-            description: 'Website for an assignment for school in the style of reddit and instagram mixed, html/css/js',
+            title: 'Rabbit (Reddit clone)',
             link: 'https://tgsoftware.services/socialmedia',
             image: 'https://styles.redditmedia.com/t5_5s5qbl/styles/communityIcon_tqrzte0yaa3c1.png',
+            description: "This was a small assignment for school creating the wwinfinite scroller like instagram, reddit wor twitter. The content is drawn from an api and loaded in infinitely.",
+            tags: "HTML, CSS, JS",
         },
         
     ];
 
+    const openPopup = (project) => {
+        setSelectedProject(project);
+        setPopupVisible(true);
+    };
+
+    const closePopup = () => {
+        setPopupVisible(false);
+        setSelectedProject(null);
+    };
+
     return (
         <section className="school">
             {projects.map((project, index) => (
-                <div key={index} className="school__card">
+                <div
+                    key={index}
+                    className="school__card"
+                    style={{ animationDelay: `${index * 0.1}s` }} // Dynamic animation delay
+                >
                     <div className='school__cardimgcontainer'>
                         <img className="school__cardImage" src={project.image} alt={`Project ${index + 1}`} />
                     </div>
                     <div className="school__cardContent">
                         <h2 className="school__cardTitle">{project.title}</h2>
-                        <p className="school__cardDescription">{project.description}</p>
-                        <a className="school__cardButton" href={project.link} target="_blank" rel="noopener noreferrer">
-                            View Project
-                        </a>
+                        <p className="school__cardTags">tags: {project.tags}</p>
+                        <button className="school__cardButton" onClick={() => openPopup(project)}>
+                            Learn more
+                        </button>
                     </div>
                 </div>
             ))}
+
+            {popupVisible && selectedProject && (
+                <div className="popup">
+                    <div className="popupContainer grower2"> 
+                        <div className="selectedImgContainer trans2">
+                            <img className="popup__cardImage" src={selectedProject.image} alt={`Project ${selectedProject.index + 1}`} /> 
+                        </div>
+                        <div className="popup__content trans2">
+                            <div className="textContainer">
+                                <h2>{selectedProject.title}</h2>
+                                <p>{selectedProject.description}</p>
+                            </div>
+                            <p className="school__cardTags">tags: <b>{selectedProject.tags}</b></p>
+                            <div className="buttonscontainer">
+                                <a className="school__cardButton" href={selectedProject.link} target="_blank" rel="noopener noreferrer">
+                                    Go to Project
+                                </a>
+                                <button className="school__cardButton" onClick={closePopup}>Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </section>
     );
 }
--- src/selfmade.js
+++ src/selfmade.js
@@ -1,56 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './school.css'; // Using the same styling
 
 function Selfmade() {
+    const [popupVisible, setPopupVisible] = useState(false);
+    const [selectedProject, setSelectedProject] = useState(null);
+
     const projects = [
         {
             title: 'Netflix Homepage clone',
-            description: 'First ever website made for improving my skills, html/css',
             link: 'https://tgsoftware.services/notflix',
             image: 'https://static.vecteezy.com/system/resources/previews/022/101/069/original/netflix-logo-transparent-free-png.png',
+            description: "Remade the Netflix homepage login / register, this was a fun project in the beginning of the first year of college to learn and teach myself how to code.",
+            tags: "HTML, CSS",
         },
         {
-            title: 'Business website Grandma/Grandpa',
-            description: 'Website made for my grandpa and grandma, html/css/js',
+            title: 'Website Grandpa',
             link: 'https://tgsoftware.services/carpediemchezmargreet',
-            image: 'https://tgsoftware.services/carpediemchezmargreet/img/about.webp',
+            image: 'https://scontent-ams4-1.xx.fbcdn.net/v/t39.30808-6/451957143_3799346643638113_5450896627723457261_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=SkkZi7CcskIQ7kNvgH89QlZ&_nc_ht=scontent-ams4-1.xx&oh=00_AYCGOPVJh17dzXlgBGb7tIQO1UUSbo9j5YhR3OVL4VH4CQ&oe=66F79D46',
+            description: "This website i made for my grandma and grandpa to improve my skills and get them a nice looking basic website for all their endevours.",
+            tags: "HTML, CSS, JS",
         },
         {
             title: 'Simple Geometry Dash',
-            description: 'Website made for improving my coding skills, html/css/js',
             link: 'https://tgsoftware.services/game',
-            image: 'https://i.pinimg.com/originals/9b/37/18/9b3718113bf81ce525f410a50e847782.png',
+            image: 'https://www1.minijuegosgratis.com/v3/games/thumbnails/213070_1.jpg',
+            description: "This is a geometry dash game but then the really basic version of it. It keeps the score but does not have a menu or anything. Build this mainly for Javascript improvement.",
+            tags: "HTML, CSS, JS",
         },
         {
-            title: 'FlavorFlow',
+            title: 'FlavorFlow.',
             description: 'Unfinished project that replaces thuisbezorgd. php/css/js',
             link: 'https://tgsoftware.services/flavorflow',
             image: 'https://cdn.pixabay.com/photo/2016/05/31/10/52/not-yet-1426593_640.png',
+            description: "This is a big project i started working on in 2023. This project was for learning more about php and javascript combined to create a seamless shopping experience. This project is still not finished due to lack of motivation. This project is using the google maps api and some other cool features. IMPORTANT! use zipcode of Medemblik the netherlands and a random street number. It's location is verified through maps.",
+            tags: "PHP, HTML, CSS, JS",
         },
         {
             title: 'Storm Services',
             description: 'Website for client, first php. php/css/js',
             link: 'https://tgsoftware.services/storm',
-            image: 'https://yt3.googleusercontent.com/NkPjOVCsOqSUA4Cyd1ET585ENqFbDHOCOy1jVLnD8J5nDiLqNNUxSQhKb1c9CoTW4Rv8uwQu=s160-c-k-c0x00ffffff-no-rj',
+            image: 'https://i.ytimg.com/vi/asTesU-2t3k/maxresdefault.jpg',
+            description: "This was my first actual website for a client named Storm, this was also my first ever php based website.",
+            tags: "PHP, HTML, CSS, JS",
         },
     ];
+
+    const openPopup = (project) => {
+        setSelectedProject(project);
+        setPopupVisible(true);
+    };
+
+    const closePopup = () => {
+        setPopupVisible(false);
+        setSelectedProject(null);
+    };
 
     return (
         <section className="school">
             {projects.map((project, index) => (
-                <div key={index} className="school__card">
+                <div
+                    key={index}
+                    className="school__card"
+                    style={{ animationDelay: `${index * 0.1}s` }} // Dynamic animation delay
+                >
                     <div className='school__cardimgcontainer'>
                         <img className="school__cardImage" src={project.image} alt={`Project ${index + 1}`} />
                     </div>
                     <div className="school__cardContent">
                         <h2 className="school__cardTitle">{project.title}</h2>
-                        <p className="school__cardDescription">{project.description}</p>
-                        <a className="school__cardButton" href={project.link} target="_blank" rel="noopener noreferrer">
-                            View Project
-                        </a>
+                        <p className="school__cardTags">tags: {project.tags}</p>
+                        <button className="school__cardButton" onClick={() => openPopup(project)}>
+                            Learn more
+                        </button>
                     </div>
                 </div>
             ))}
+
+            {popupVisible && selectedProject && (
+                <div className="popup">
+                    <div className="popupContainer grower2"> 
+                        <div className="selectedImgContainer trans2">
+                            <img className="popup__cardImage" src={selectedProject.image} alt={`Project ${selectedProject.index + 1}`} /> 
+                        </div>
+                        <div className="popup__content trans2">
+                            <div className="textContainer">
+                                <h2>{selectedProject.title}</h2>
+                                <p>{selectedProject.description}</p>
+                            </div>
+                            <p className="school__cardTags">tags: <b>{selectedProject.tags}</b></p>
+                            <div className="buttonscontainer">
+                                <a className="school__cardButton" href={selectedProject.link} target="_blank" rel="noopener noreferrer">
+                                    Go to Project
+                                </a>
+                                <button className="school__cardButton" onClick={closePopup}>Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </section>
     );
 }
