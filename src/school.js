6b89e8f59b8d7dc63384b0d6ce80d584b5edf154
Jedemajed <90614493+Jedemajed4617@users.noreply.github.com>
2024-10-02 17:45:02 +0200

    Update #1.7.0 - Filters
    - Added tag filters to the project pages
    - Updated about css code
    - Updated skills css code

--- src/school.js
+++ src/school.js
@@ -1,37 +1,39 @@
 import React, { useState } from 'react';
-import './school.css'; 
+import './school.css';
 
 function School() {
     const [popupVisible, setPopupVisible] = useState(false);
     const [selectedProject, setSelectedProject] = useState(null);
+    const [selectedTags, setSelectedTags] = useState([]);
+    const [showFilters, setShowFilters] = useState(false); // Manage dropdown visibility
 
     const projects = [
         {
             title: 'Spatify (spotify clone)',
             link: 'https://tgsoftware.services/spatify',
             image: 'https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs=w240-h480-rw',
-            description: "This is a website a made for a school assignment. This website has it's own small library of songs that you can acces. This was to train my php skills in sorting, searching and adding songs with a small custom csm system.",
-            tags: "PHP, HTML, CSS, JS",
+            description: "This is a website a made for a school assignment. This website has it's own small library of songs that you can access. This was to train my PHP skills in sorting, searching and adding songs with a small custom CMS system.",
+            tags: "PHP, CSS, JS",
         },
         {
             title: 'Daily Paper Clone',
             link: 'https://tgsoftware.services/dailypaper',
             image: 'https://www.asphaltgold.com/cdn/shop/files/ca802008830dc007677dcccde8cf41e179b52d66_2322035_Daily_Paper_Ezar_Zip_Hoodie_Black_os_3_768x768.jpg?v=1700042914',
-            description: "This was an assignment for school I made with php, This is a full scale daily paper clone with working cms, login, dipuste system, review system and much more. Had so much fun creating this prject because it enhanced my capability to write php.",
-            tags: "PHP, HTML, CSS, JS",
+            description: "This was an assignment for school I made with PHP. This is a full scale daily paper clone with working CMS, login, dispute system, review system and much more. Had so much fun creating this project because it enhanced my capability to write PHP.",
+            tags: "PHP, CSS, JS",
         },
         {
             title: 'Tulpreizen',
             link: 'https://tgsoftware.services/tulpreizen',
             image: 'https://www.whiteflowerfarm.com/mas_assets/cache/image/9/4/e/b/38123.Jpg',
-            description: "This was a small website for school that shows of a bit of scss skills i used for it. Nothing much to it other then nice!",
+            description: "This was a small website for school that shows off a bit of SCSS skills I used for it. Nothing much to it other than nice!",
             tags: "HTML, SCSS, CSS",
         },
         {
             title: 'Font Showcase',
             link: 'https://tgsoftware.services/font-showcase',
             image: 'https://img.freepik.com/free-vector/creative-halloween-alphabet-design_23-2147932875.jpg?size=338&ext=jpg&ga=GA1.1.1413502914.1696809600&semt=ais',
-            description: "This was a website created for previewing different font styles with js. Anything you type into the bar will show it in the font listed.",
+            description: "This was a website created for previewing different font styles with JS. Anything you type into the bar will show it in the font listed.",
             tags: "HTML, CSS, JS",
         },
         {
@@ -39,24 +41,25 @@ function School() {
             link: 'https://tgsoftware.services/nvvn',
             image: 'https://nvvn.nl/wp-content/uploads/2021/06/NVVN-favicon.png',
             description: "This is a website made for the official NVVN. We needed to show what we had learned the previous years and just make a website for them.",
-            tags: "PHP, HTML, CSS, JS",
+            tags: "PHP, CSS, JS",
         },
         {
             title: 'Qr Code',
             link: 'https://tgsoftware.services/qrcode',
             image: 'https://media.istockphoto.com/id/1347277567/vector/qr-code-sample-for-smartphone-scanning-on-white-background.jpg?s=612x612&w=0&k=20&c=PYhWHZ7bMECGZ1fZzi_-is0rp4ZQ7abxbdH_fm8SP7Q=',
-            description: "This was a really fun project. This website i made could generate qr codes after you had taken a picture with any device on the website and the if you had scan the QR-code you were redirected to a different page to collect your photo.",
-            tags: "PHP, HMTL, CSS, JS",
+            description: "This was a really fun project. This website I made could generate QR codes after you had taken a picture with any device on the website and if you had scanned the QR code you were redirected to a different page to collect your photo.",
+            tags: "PHP, CSS, JS",
         },
         {
             title: 'Rabbit (Reddit clone)',
             link: 'https://tgsoftware.services/socialmedia',
             image: 'https://styles.redditmedia.com/t5_5s5qbl/styles/communityIcon_tqrzte0yaa3c1.png',
-            description: "This was a small assignment for school creating the infinite scroller like instagram, reddit or twitter. The content is drawn from an api and loaded in infinitely.",
+            description: "This was a small assignment for school creating the infinite scroller like Instagram, Reddit or Twitter. The content is drawn from an API and loaded in infinitely.",
             tags: "HTML, CSS, JS",
         },
-        
     ];
+
+    const tags = ["HTML", "CSS", "JS", "PHP"]; // Define the available tags
 
     const openPopup = (project) => {
         setSelectedProject(project);
@@ -68,46 +71,70 @@ function School() {
         setSelectedProject(null);
     };
 
+    const handleTagChange = (tag) => {
+        setSelectedTags(prevTags =>
+            prevTags.includes(tag)
+                ? prevTags.filter(t => t !== tag) // Remove tag if already selected
+                : [...prevTags, tag] // Add tag if not selected
+        );
+    };
+
+    const filteredProjects = selectedTags.length === 0
+        ? projects // Show all projects if no tag is selected
+        : projects.filter(project => 
+            selectedTags.some(tag => project.tags.includes(tag)) // Change here to use some instead of every
+        );
+
     return (
         <section className="school">
-            {projects.map((project, index) => (
-                <div
-                    key={index}
-                    className="school__card"
-                    style={{ animationDelay: `${index * 0.1}s` }} // Dynamic animation delay
-                >
-                    <div className='school__cardimgcontainer'>
-                        <img className="school__cardImage" src={project.image} alt={`Project ${index + 1}`} />
+            <div className="school__filters">
+                <button className="dropdown-filtertoggle" onClick={() => setShowFilters(!showFilters)}>
+                    Filter by tags &#9662;
+                </button>
+                {showFilters && (
+                    <div className="dropdown-filters">
+                        {tags.map(tag => (
+                            <label key={tag} className="filters">
+                                <input
+                                    type="checkbox"
+                                    checked={selectedTags.includes(tag)}
+                                    onChange={() => handleTagChange(tag)}
+                                />
+                                {tag}
+                            </label>
+                        ))}
                     </div>
-                    <div className="school__cardContent">
-                        <h2 className="school__cardTitle">{project.title}</h2>
-                        <p className="school__cardTags">tags: {project.tags}</p>
-                        <button className="school__cardButton" onClick={() => openPopup(project)}>
-                            Learn more
-                        </button>
+                )}
+            </div>
+
+            <div className="school__divcardcontainer">
+                {filteredProjects.map((project, index) => (
+                    <div
+                        key={index}
+                        className="school__card"
+                        style={{ animationDelay: `${index * 0.1}s` }} // Dynamic animation delay
+                    >
+                        <div className='school__cardimgcontainer'>
+                            <img className="school__cardImage" src={project.image} alt={`Project ${index + 1}`} />
+                        </div>
+                        <div className="school__cardContent">
+                            <h2 className="school__cardTitle">{project.title}</h2>
+                            <p className="school__cardTags">tags: <b>{project.tags}</b></p>
+                            <button className="school__cardButton" onClick={() => openPopup(project)}>
+                                Learn more
+                            </button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
 
             {popupVisible && selectedProject && (
                 <div className="popup">
-                    <div className="popupContainer grower2"> 
-                        <div className="selectedImgContainer trans2">
-                            <img className="popup__cardImage" src={selectedProject.image} alt={`Project ${selectedProject.index + 1}`} /> 
-                        </div>
-                        <div className="popup__content trans2">
-                            <div className="textContainer">
-                                <h2>{selectedProject.title}</h2>
-                                <p>{selectedProject.description}</p>
-                            </div>
-                            <p className="school__cardTags">tags: <b>{selectedProject.tags}</b></p>
-                            <div className="buttonscontainer">
-                                <a className="school__cardButton" href={selectedProject.link} target="_blank" rel="noopener noreferrer">
-                                    Go to Project
-                                </a>
-                                <button className="school__cardButton" onClick={closePopup}>Close</button>
-                            </div>
-                        </div>
+                    <div className="popup__content">
+                        <h2>{selectedProject.title}</h2>
+                        <img src={selectedProject.image} alt={selectedProject.title} />
+                        <p>{selectedProject.description}</p>
+                        <button onClick={closePopup}>Close</button>
                     </div>
                 </div>
             )}
--- src/selfmade.js
+++ src/selfmade.js
@@ -1,47 +1,21 @@
 import React, { useState } from 'react';
-import './school.css'; // Using the same styling
+import './school.css';
 
 function Selfmade() {
     const [popupVisible, setPopupVisible] = useState(false);
     const [selectedProject, setSelectedProject] = useState(null);
+    const [selectedTags, setSelectedTags] = useState([]);
+    const [showFilters, setShowFilters] = useState(false); // Add this line to manage dropdown visibility
 
     const projects = [
-        {
-            title: 'Netflix Homepage clone',
-            link: 'https://tgsoftware.services/netflix',
-            image: 'https://static.vecteezy.com/system/resources/previews/022/101/069/original/netflix-logo-transparent-free-png.png',
-            description: "Remade the Netflix homepage login / register, this was a fun project in the beginning of the first year of college to learn and teach myself how to code.",
-            tags: "HTML, CSS",
-        },
-        {
-            title: 'Website Grandpa',
-            link: 'https://tgsoftware.services/carpediemchezmargreet',
-            image: 'https://scontent-ams4-1.xx.fbcdn.net/v/t39.30808-6/451957143_3799346643638113_5450896627723457261_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=SkkZi7CcskIQ7kNvgH89QlZ&_nc_ht=scontent-ams4-1.xx&oh=00_AYCGOPVJh17dzXlgBGb7tIQO1UUSbo9j5YhR3OVL4VH4CQ&oe=66F79D46',
-            description: "This website i made for my grandma and grandpa to improve my skills and get them a nice looking basic website for all their endevours.",
-            tags: "HTML, CSS, JS",
-        },
-        {
-            title: 'Simple Geometry Dash',
-            link: 'https://tgsoftware.services/game',
-            image: 'https://www1.minijuegosgratis.com/v3/games/thumbnails/213070_1.jpg',
-            description: "This is a geometry dash game but then the really basic version of it. It keeps the score but does not have a menu or anything. Build this mainly for Javascript improvement.",
-            tags: "HTML, CSS, JS",
-        },
-        {
-            title: 'FlavorFlow.',
-            link: 'https://tgsoftware.services/flavorflow',
-            image: 'https://cdn.pixabay.com/photo/2016/05/31/10/52/not-yet-1426593_640.png',
-            description: "This is a big project i started working on in 2023. This project was for learning more about php and javascript combined to create a seamless shopping experience. This project is using the google maps api and some other cool features. IMPORTANT! use zipcode of Medemblik the netherlands and a random street number. It's location is verified through maps.",
-            tags: "PHP, HTML, CSS, JS",
-        },
-        {
-            title: 'Storm Services',
-            link: 'https://tgsoftware.services/storm',
-            image: 'https://i.ytimg.com/vi/asTesU-2t3k/maxresdefault.jpg',
-            description: "This was my first actual website for a client named Storm, this was also my first ever php based website.",
-            tags: "PHP, HTML, CSS, JS",
-        },
+        { title: 'Netflix', link: 'https://tgsoftware.services/netflix', image: 'https://static.vecteezy.com/system/resources/previews/022/101/069/original/netflix-logo-transparent-free-png.png', description: "Remade the Netflix homepage login / register...", tags: "HTML, CSS" },
+        { title: 'Website Grandpa', link: 'https://tgsoftware.services/carpediemchezmargreet', image: 'https://scontent-ams4-1.xx.fbcdn.net/v/t39.30808-6/451957143_3799346643638113_5450896627723457261_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=SkkZi7CcskIQ7kNvgH89QlZ&_nc_ht=scontent-ams4-1.xx&oh=00_AYCGOPVJh17dzXlgBGb7tIQO1UUSbo9j5YhR3OVL4VH4CQ&oe=66F79D46', description: "This website I made for my grandparents...", tags: "HTML, CSS, JS" },
+        { title: 'Simple Geometry Dash', link: 'https://tgsoftware.services/game', image: 'https://www1.minijuegosgratis.com/v3/games/thumbnails/213070_1.jpg', description: "This is a basic version of Geometry Dash...", tags: "HTML, CSS, JS" },
+        { title: 'FlavorFlow', link: 'https://tgsoftware.services/flavorflow', image: 'https://cdn.pixabay.com/photo/2016/05/31/10/52/not-yet-1426593_640.png', description: "This is a project to learn PHP and JS integration...", tags: "PHP, CSS, JS" },
+        { title: 'Storm Services', link: 'https://tgsoftware.services/storm', image: 'https://i.ytimg.com/vi/asTesU-2t3k/maxresdefault.jpg', description: "First website for a client using PHP...", tags: "PHP, CSS, JS" },
     ];
+
+    const tags = ["HTML", "CSS", "JS", "PHP"]; // Define the available tags
 
     const openPopup = (project) => {
         setSelectedProject(project);
@@ -53,49 +27,62 @@ function Selfmade() {
         setSelectedProject(null);
     };
 
+    const handleTagChange = (tag) => {
+        setSelectedTags(prevTags =>
+            prevTags.includes(tag)
+                ? prevTags.filter(t => t !== tag) // Remove tag if already selected
+                : [...prevTags, tag] // Add tag if not selected
+        );
+    };
+
+    const filteredProjects = selectedTags.length === 0
+        ? projects // Show all projects if no tag is selected
+        : projects.filter(project => 
+            selectedTags.some(tag => project.tags.includes(tag)) // Change here to use some instead of every
+        );
+
     return (
         <section className="school">
-            {projects.map((project, index) => (
+            <div className="school__filters">
+                <button className="dropdown-filtertoggle" onClick={() => setShowFilters(!showFilters)}>
+                    Filter by tags &#9662;
+                </button>
+                {showFilters && (
+                    <div className="dropdown-filters">
+                        {tags.map(tag => (
+                            <label key={tag} className="filters">
+                                <input
+                                    type="checkbox"
+                                    checked={selectedTags.includes(tag)}
+                                    onChange={() => handleTagChange(tag)}
+                                />
+                                {tag}
+                            </label>
+                        ))}
+                    </div>
+                )}
+            </div>
+
+            <div className="school__divcardcontainer">
+            {filteredProjects.map((project, index) => (
                 <div
                     key={index}
                     className="school__card"
-                    style={{ animationDelay: `${index * 0.1}s` }} // Dynamic animation delay
+                    style={{ animationDelay: `${index * 0.1}s` }}
                 >
                     <div className='school__cardimgcontainer'>
                         <img className="school__cardImage" src={project.image} alt={`Project ${index + 1}`} />
                     </div>
                     <div className="school__cardContent">
                         <h2 className="school__cardTitle">{project.title}</h2>
-                        <p className="school__cardTags">tags: {project.tags}</p>
+                        <p className="school__cardTags">tags: <b>{project.tags}</b></p>
                         <button className="school__cardButton" onClick={() => openPopup(project)}>
                             Learn more
                         </button>
                     </div>
                 </div>
             ))}
-
-            {popupVisible && selectedProject && (
-                <div className="popup">
-                    <div className="popupContainer grower2"> 
-                        <div className="selectedImgContainer trans2">
-                            <img className="popup__cardImage" src={selectedProject.image} alt={`Project ${selectedProject.index + 1}`} /> 
-                        </div>
-                        <div className="popup__content trans2">
-                            <div className="textContainer">
-                                <h2>{selectedProject.title}</h2>
-                                <p>{selectedProject.description}</p>
-                            </div>
-                            <p className="school__cardTags">tags: <b>{selectedProject.tags}</b></p>
-                            <div className="buttonscontainer">
-                                <a className="school__cardButton" href={selectedProject.link} target="_blank" rel="noopener noreferrer">
-                                    Go to Project
-                                </a>
-                                <button className="school__cardButton" onClick={closePopup}>Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )}
+            </div>
         </section>
     );
 }
