ad2198b95b915376f033b19035f732852271f747
Jedemajed4617 <90614493+Jedemajed4617@users.noreply.github.com>
2025-02-27 15:32:16 +0100

    Small portfolio update

--- src/about.js
+++ src/about.js
@@ -4,6 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFaceLaugh, faClipboard, faCompass, faNewspaper } from "@fortawesome/free-regular-svg-icons";
 
 function About() {
+    const geboortedatum = '2003-02-22'; // Gebruik het formaat YYYY-MM-DD
+    const [leeftijd, setLeeftijd] = useState(berekenLeeftijd(geboortedatum));
+  
+    useEffect(() => {
+      setLeeftijd(berekenLeeftijd(geboortedatum));
+    }, [geboortedatum]);
     return (
         <section className="about">
             <div className="about__contentManagement">   
@@ -14,8 +20,8 @@ function About() {
                     <div className="about__contentTextArea">
                         <h1 className="about__contentTitle">Introductie:</h1>
                         <p className="about__contentInfo">
-                            Mijn naam is Tygo. Ik ben 21 jaar oud en volg momenteel een opleiding tot full stack software developer. 
-                            In mijn vrije tijd geniet ik van fitness, golf, tennis en coderen.
+                            Mijn naam is Tygo. Ik ben {leeftijd} jaar oud en volg momenteel een opleiding tot full stack software developer. 
+                            Ik ben geïnteresseerd in het ontwikkelen van webapplicaties en het leren van nieuwe programmeertalen.
                         </p>
                     </div>
                 </li>
@@ -28,7 +34,7 @@ function About() {
                     <div className="about__contentTextArea">
                         <h1 className="about__contentTitle">Wat ik wil bereiken:</h1>
                         <p className="about__contentInfo">
-                            Ik streef ernaar om zoveel mogelijk programmeertalen te leren en uitgebreide kennis op te doen over het vakgebied waarin ik momenteel studeer. 
+                            Ik streef ernaar om zoveel mogelijk programmeertalen te leren en uitgebreid kennis op te doen over het vakgebied waarin ik momenteel studeer. 
                             Daarnaast ben ik geïnteresseerd in de zakelijke kant van softwareontwikkeling, met de ambitie om in de toekomst mijn eigen bedrijf te starten.
                         </p>
                     </div>
@@ -43,7 +49,7 @@ function About() {
                         <h1 className="about__contentTitle">Wat ik wil leren:</h1>
                         <p className="about__contentInfo">
                             Op dit moment richt ik me op het verbeteren van mijn code door deze duidelijker en efficiënter te schrijven. 
-                            Ik wil graag projecten ontwikkelen met Lua, Laravel en React, en me verder verdiepen in de zakelijke aspecten van de softwareontwikkeling.
+                            Ik wil graag projecten ontwikkelen met Laravel en React, en me verder verdiepen in de zakelijke aspecten van de softwareontwikkeling.
                         </p>
                     </div>
                 </li>
@@ -57,7 +63,7 @@ function About() {
                         <h1 className="about__contentTitle">Over mij</h1>
                         <p className="about__contentInfo">
                             Naast fitness en golf vind ik het ook leuk om te gamen in mijn vrije tijd. 
-                            Ik woon nog bij mijn ouders in Medemblik, samen met onze twee honden, in een gezellig huis in het centrum.
+                            Ik woon nog bij mijn ouders in Medemblik, en ben van plan om in de toekomst te verhuizen naar het buitenland.
                         </p>
                     </div>
                 </li> 
